e6204199cf20c36a988544c1e1f3fac2664f7934
Fatih20823 <101557027+Fatih20823@users.noreply.github.com>
2022-12-07 15:56:53 +0300

    first commit

---
+++ src/com/main/Application.java
@@ -0,0 +1,23 @@
+package com.main;
+
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+	}
+
+}
+ class MyRegex {
+
+	 public String pattern = "((\\d|\\d\\d|[0-1]\\d\\d|2[0-4][0-9]|25[0-5])\\.){3}(\\d|\\d\\d|[0-1]\\d\\d|2[0-4][0-9]|25[0-5])";
+	
+	
+}
+
